8764137a04d1dccc2c86ae88808f5ee90ea90563
LeviTC <erletaco@outlook.com>
2026-04-16 11:14:36 -0600

    chore(app): initializing project

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./presentation/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#49129C",
+        secondary: {
+          DEFAULT: "#B40086",
+          100: "#C51297",
+          200: "#831266",
+        },
+        tertiary: "#EF2967",
+      }
+
+    },
+  },
+  plugins: [],
+}
